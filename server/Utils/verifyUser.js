0a2dac82687916915cae9f9f79315677b44ff030
TARANSH CHELLANI <taranshchellani121@gmail.com>
2024-01-16 03:48:43 +0530

    jwt fix

--- server/Utils/verifyUser.js
+++ server/Utils/verifyUser.js
@@ -7,7 +7,7 @@ export const verifyToken = (req, res, next) => {
 
     if (!token) return next(errorHandler(401, 'Unauthorizedd'));
 
-    jwt.verify(token, process.env.JWT_Token, (err, user) => {
+    jwt.verify(token, process.env.Jwt_Token, (err, user) => {
         if (err) return next(errorHandler(403, 'Forbidden'));
 
         req.user = user;
