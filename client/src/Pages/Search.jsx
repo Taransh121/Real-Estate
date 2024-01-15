d21aebc368f7145f160509e429a807a62efe0754
TARANSH CHELLANI <taranshchellani121@gmail.com>
2024-01-16 02:58:41 +0530

    Completed..

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import ListingItem from '../Components/listingItem';
+import ListingItem from '../Components/ListingItem';
 
 export default function Search() {
     const navigate = useNavigate();
