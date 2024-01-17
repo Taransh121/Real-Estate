fa68de18c062c8038af22ec2ccb643edca0e73d2
TARANSH CHELLANI <taranshchellani121@gmail.com>
2024-01-17 22:44:39 +0530

    done.

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -113,7 +113,7 @@ export default function Search() {
         const urlParams = new URLSearchParams(location.search);
         urlParams.set("startIndex", startIndex);
         const searchQuery = urlParams.toString();
-        const res = await fetch(`http://localhost:8080/api/listing/get?${searchQuery}`);
+        const res = await fetch(`https://real-estate-bokm.onrender.com/api/listing/get?${searchQuery}`);
         const data = await res.json();
         if (data.length < 8) {
             setShowMore(false);
